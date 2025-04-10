65ecc412e6f191677e5b8b56e7efe237c84b44c3
Rahul <rahulkumarb4ubxr@gmail.com>
2025-04-10 11:51:47 +0530

    Initial commit

---
+++ src/test/java/com/rahulkrbxr/rest/webservices/resetful_web_services/ResetfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulkrbxr.rest.webservices.resetful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResetfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

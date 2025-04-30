50562e27a0fc9feffdef0d847741bb751cf89d50
Rahul <rahulkumarb4ubxr@gmail.com>
2025-04-30 18:35:14 +0530

    Content Negotiation - Implementing Support for XML Internationalization for REST API

--- src/main/java/com/rahul/resetfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rahul/resetfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.rahul.resetfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -20,5 +30,16 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default message", locale);
+		
+//		- Example: `en` - English (Good Morning)
+//		- Example: `nl` - Dutch (Goedemorgen)
+//		- Example: `fr` - French (Bonjour)
+//		- Example: `de` - Deutsch (Guten Morgen)
 	}
 }
